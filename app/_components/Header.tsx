5aad747ef7e1fd36e212de61e082886d8b0ad37e
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-06-04 23:15:51 -0700

    style Header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,51 +1,22 @@
 import {
-    Box,
-    Button,
-    Center,
     Flex,
     Heading,
-    Input,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    Spacer } from '@chakra-ui/react'
+    Input } from '@chakra-ui/react'
 
 export default function Header() {
     return (
-        <Flex justify='center'>
-            <Center>
-                <Heading>Reddit Video Games Feed</Heading>
-            </Center>
-            <Spacer />
-            <Box>
-                <Input variant='outline' placeholder='Search' size='md' />
-            </Box>
-            <Spacer />
-            <Menu>
-                <MenuButton as={Button}>
-                    Subreddits
-                </MenuButton>
-                <MenuList>
-                    <MenuItem>r/AndroidGaming</MenuItem>
-                    <MenuItem>r/emulation</MenuItem>
-                    <MenuItem>r/gamecollecting</MenuItem>
-                    <MenuItem>r/gamemusic</MenuItem>
-                    <MenuItem>r/gamephysics</MenuItem>
-                    <MenuItem>r/gamernews</MenuItem>
-                    <MenuItem>r/Games</MenuItem>
-                    <MenuItem>r/gaming4gamers</MenuItem>
-                    <MenuItem>r/gamingdetails</MenuItem>
-                    <MenuItem>r/IndieGaming</MenuItem>
-                    <MenuItem>r/MMORPG</MenuItem>
-                    <MenuItem>r/patientgamers</MenuItem>
-                    <MenuItem>r/randomactsofgaming</MenuItem>
-                    <MenuItem>r/retrogaming</MenuItem>
-                    <MenuItem>r/speedrun</MenuItem>
-                    <MenuItem>r/truegaming</MenuItem>
-                    <MenuItem>r/webgames</MenuItem>
-                </MenuList>
-                </Menu>
+        <Flex justify='center' mx={50} my={15}>
+            <Flex
+                direction='row'
+                justify='space-between'
+                align='center' maxW='1200px'
+                width='100%'
+                borderBottom='solid thin grey'
+                pb={15}
+            >
+                <Heading as='h1'>Reddit Video Games Feed</Heading>
+                <Input variant='outline' placeholder='Search' size='lg' width={300} height={50} borderRadius={15}/>
+            </Flex>
         </Flex>
     )
 }
