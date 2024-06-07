69e3cb9471103f84c5ec9d5190d1af9f45660c29
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-06-07 08:31:22 -0700

    add Links to SideNav buttons

--- app/_components/SideNav.tsx
+++ app/_components/SideNav.tsx
@@ -4,6 +4,8 @@ import {
     Heading,
     Stack } from '@chakra-ui/react';
 
+import Link from 'next/link';
+
 export default function SideNav() {
     return (
         <Flex
@@ -13,26 +15,27 @@ export default function SideNav() {
             borderRight='solid thin #d7d7d7'
             height='100vh'
             direction='column'
+            overflowX='scroll'
         >
             <Heading as='h2' size='sm' py={5} textAlign='center'>Subreddits</Heading>
             <Stack spacing={5} align='start'>
-                <Button variant='ghost' w='100%' size='sm'>r/AndroidGaming</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/emulation</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/gamecollecting</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/gamemusic</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/gamephysics</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/gamernews</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/Games</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/gaming4gamers</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/gamingdetails</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/IndieGaming</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/MMORPG</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/patientgamers</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/randomactsofgaming</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/retrogaming</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/speedrun</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/truegaming</Button>
-                <Button variant='ghost' w='100%' size='sm'>r/webgames</Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/android-gaming'>r/AndroidGaming</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/emulation'>r/emulation</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/game-collecting'>r/gamecollecting</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/game-music'>r/gamemusic</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/game-physics'>r/gamephysics</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/gamer-news'>r/gamernews</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/games'>r/Games</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/gaming-4-gamers'>r/gaming4gamers</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/gaming-details'>r/gamingdetails</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/indie-gaming'>r/IndieGaming</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/mmorpg'>r/MMORPG</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/patient-gamers'>r/patientgamers</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/random-acts-of-gaming'>r/randomactsofgaming</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/retro-gaming'>r/retrogaming</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/speedrun'>r/speedrun</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/true-gaming'>r/truegaming</Link></Button>
+                <Button variant='ghost' w='100%' size='sm'><Link href='/web-games'>r/webgames</Link></Button>
             </Stack>
         </Flex>
     )
