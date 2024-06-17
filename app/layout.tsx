bf329ad7ba0128ac63591d9c2164bd5dafc826a3
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-06-17 01:05:13 -0700

    fix: remove white space bounce when scrolling to the top or bottom of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from './providers';
 import { Flex } from '@chakra-ui/react'
 import Header from './_components/Header';
 import SideNav from './_components/SideNav';
+import './globals.css';
 
 export default function RootLayout({
   children,
