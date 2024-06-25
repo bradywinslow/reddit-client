26b96a6222aa1383973811db53b99f5f1eb36ba2
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-06-24 22:56:27 -0700

    delete old code that's no longer needed

--- app/reddit/httpRequests.ts
+++ app/reddit/httpRequests.ts
@@ -1,16 +1,3 @@
-/* export default async function getSubredditData({ params }: { params: { page: string } }) {
-    const res = await fetch(`http://localhost:3000/${params.page}`);
-    const data = await res.json();
-    return data.data;
-} */
-
-
-/* export default async function getSubredditData(page: { params: { page: string; }; }) {
-    const res = await fetch(`https://www.reddit.com/r/${page}.json`);
-    const data = await res.json();
-    return data;
-} */
-
 export default async function getSubredditData({ params }: { params: { page: string } }) {
     const urlToFetch = `https://www.reddit.com/r/${params.page}.json`;
 
