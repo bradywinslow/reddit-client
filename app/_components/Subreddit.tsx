65ac4f9740d8c45684e36ce472b146f7f2468c6d
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-07-25 23:54:30 -0700

    reposts no longer have a gap in their card for the reposted text

--- app/_components/Subreddit.tsx
+++ app/_components/Subreddit.tsx
@@ -81,7 +81,7 @@ const Subreddit: React.FC<SubredditProps> = ({ page, subredditName }) => {
 
                     <Flex direction='column' align='center'>
                         <Box w={[175, 200, 225, 250]} m={5}>
-                            <Skeleton h='40px' borderRadius={15} />
+                            <Skeleton h='40px' />
                         </Box>
                     </Flex>
                     <Box gap='2px'>
@@ -109,12 +109,6 @@ const Subreddit: React.FC<SubredditProps> = ({ page, subredditName }) => {
                                 const currentDate = new Date(0);
                                 const timeElapsed = formatDistance(currentDate.setUTCSeconds(timestamp), Date.now(), { addSuffix: true});
 
-                                // If media content in post is YouTube video, extract url
-                                const youTubeIframeString: string = postData.media_embed?.content;
-                                // Regular expression to extract YouTube url
-                                const regex = /scr=\/"(.*?)" enablejsapi=1\\/;
-                                const youTubeUrl = youTubeIframeString.match(regex);
-
                                 return (
                                     <Card w={[200, 400, 500, 700]} key={index} mb={7} px={['2px', '9px', '16px']} gap='2px'>
                                         <CardHeader>
@@ -131,70 +125,59 @@ const Subreddit: React.FC<SubredditProps> = ({ page, subredditName }) => {
                                         </CardHeader>
 
                                         <CardBody>
-                                                <Flex direction='column' gap={5}>
-                                                    <Flex direction='column' w='100%'>
-                                                        <Heading as='h4' size='sm'>
-                                                            <ReactMarkdown
-                                                                remarkPlugins={[remarkGfm, remarkRehype]}
-                                                                rehypePlugins={[rehypeReact, rehypeRaw]}
-                                                            >
-                                                                {postData.title}
-                                                            </ReactMarkdown>
-                                                        </Heading>
-                                                        <Text mt={15} width='auto' fontSize='14px'>
-                                                            <ReactMarkdown
-                                                                remarkPlugins={[remarkGfm, remarkRehype]}
-                                                                rehypePlugins={[rehypeReact, rehypeRaw]}
-                                                            >
-                                                                {postData.selftext}
-                                                            </ReactMarkdown>
-                                                        </Text>
-                                                    </Flex>
-                                                    {postData.secure_media?.reddit_video?.fallback_url || postData.secure_media?.oembed?.html  && (
-                                                        <Flex justify='center' alignItems='center'>
-                                                            <a
-                                                                href={postData.url}
-                                                                target='_blank'
-                                                                rel='noopener noreferrer'
-                                                            >
-                                                                {postData.secure_media?.reddit_video?.fallback_url ? (
-                                                                    <iframe
-                                                                        src={postData.secure_media?.reddit_video?.fallback_url}
-                                                                        width='300px'
-                                                                        height='187px'
-                                                                        allowFullScreen
-                                                                    />
-                                                                ) : (
-                                                                    <iframe
-                                                                        src={youTubeUrl}
-                                                                        width='300px'
-                                                                        height='187px'
-                                                                        allowFullScreen
-                                                                    />  
-                                                                )}
-                                                                
-                                                            </a>
-                                                        </Flex>
-                                                    )}
-                                                    {!postData.secure_media?.reddit_video?.fallback_url && postData.thumbnail && (
-                                                        <Flex justify='center' alignItems='center'>
-                                                            <a
-                                                                href={postData.url}
-                                                                target='_blank'
-                                                                rel='noopener noreferrer'
-                                                            >
-                                                                <Image
-                                                                    src={postData.thumbnail}
-                                                                    alt=''
-                                                                    borderRadius='7px'
-                                                                    h='150px'
-                                                                    w='auto'
-                                                                />
-                                                            </a>
-                                                        </Flex>
-                                                    )}
-                                                    {postData.thumbnail === 'self' && (null)}
+                                            <Flex direction='column' gap={5}>
+                                                <Flex direction='column' w='100%'>
+                                                    <Heading as='h4' size='sm'>
+                                                        <ReactMarkdown
+                                                            remarkPlugins={[remarkGfm, remarkRehype]}
+                                                            rehypePlugins={[rehypeReact, rehypeRaw]}
+                                                        >
+                                                            {postData.title}
+                                                        </ReactMarkdown>
+                                                    </Heading>
+                                                    <Text mt={15} width='auto' fontSize='14px'>
+                                                        <ReactMarkdown
+                                                            remarkPlugins={[remarkGfm, remarkRehype]}
+                                                            rehypePlugins={[rehypeReact, rehypeRaw]}
+                                                        >
+                                                            {postData.selftext}
+                                                        </ReactMarkdown>
+                                                    </Text>
                                                 </Flex>
+                                                {postData.secure_media?.reddit_video?.fallback_url && (
+                                                    <Flex justify='center' alignItems='center'>
+                                                        <a
+                                                            href={postData.url}
+                                                            target='_blank'
+                                                            rel='noopener noreferrer'
+                                                        >
+                                                            <iframe
+                                                                src={postData.secure_media?.reddit_video?.fallback_url}
+                                                                width='300px'
+                                                                height='187px'
+                                                                allowFullScreen
+                                                            />                                                               
+                                                        </a>
+                                                    </Flex>
+                                                )}
+                                                {!postData.secure_media?.reddit_video?.fallback_url && postData.thumbnail !== 'self' && postData.thumbnail !== 'default' && (
+                                                    <Flex justify='center' alignItems='center'>
+                                                        <a
+                                                            href={postData.url}
+                                                            target='_blank'
+                                                            rel='noopener noreferrer'
+                                                        >
+                                                            <Image
+                                                                src={postData.thumbnail}
+                                                                alt=''
+                                                                borderRadius='7px'
+                                                                h='150px'
+                                                                w='auto'
+                                                            />
+                                                        </a>
+                                                    </Flex>
+                                                )}
+                                            </Flex>
                                         </CardBody>
 
                                         <CardFooter
