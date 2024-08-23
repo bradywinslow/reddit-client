5414208763dff382e74fa27a2ed3d86f9490d06d
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-08-23 14:42:29 -0700

    only show mobile skeleton and not desktop sidebar when navigating to a new page on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Providers } from './providers';
+import React, { useState, useEffect } from 'react';
 import { Flex, useMediaQuery } from '@chakra-ui/react';
 import DesktopHeader from './_components/DesktopHeader';
 import MobileHeader from './_components/MobileHeader';
@@ -8,41 +9,50 @@ import SideNav from './_components/SideNav';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
   const [isMobile] = useMediaQuery("(max-width: 48em)");
-  
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <html lang="en">
       <body>
         <Providers>
-
-            <Flex
-              direction='column'
-              h='100vh'
-              display={isMobile ? "none" : "flex"}
-            >
-              <DesktopHeader />
-              <Flex direction='row' flex='1' overflow='hidden'>
-                <Flex width='250px' flexShrink={0}>
-                  <SideNav />
-                </Flex>
-                <Flex justify='center' flex='1' overflowY='auto' mt='75px'>
-                  {children}
-                </Flex>
+          
+          {/* Desktop view */}
+          <Flex
+            direction='column'
+            h='100vh'
+            display={isMobile ? "none" : "flex"}
+          >
+            <DesktopHeader />
+            <Flex direction='row' flex='1' overflow='hidden'>
+              <Flex width='250px' flexShrink={0}>
+                <SideNav />
+              </Flex>
+              <Flex justify='center' flex='1' overflowY='auto' mt='75px'>
+                {children}
               </Flex>
             </Flex>
+          </Flex>
 
-            <Flex
-              direction='column'
-              h='100vh'
-              display={!isMobile ? "none" : "flex"}
-            >
-              <MobileHeader />
-              <Flex direction='row' flex='1' overflow='hidden'>
-                <Flex justify='center' flex='1' overflowY='auto' mt='75px'>
-                  {children}
-                </Flex>
+          {/* Mobile view */}
+          <Flex
+            direction='column'
+            h='100vh'
+            display={!isMobile ? "none" : "flex"}
+          >
+            <MobileHeader />
+            <Flex direction='row' flex='1' overflow='hidden'>
+              <Flex justify='center' flex='1' overflowY='auto' mt='75px'>
+                {children}
               </Flex>
             </Flex>
+          </Flex>
 
         </Providers>
       </body>
