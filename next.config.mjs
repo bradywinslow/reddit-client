6bdbd5e438808aaea15e8780fabd2f64d4e9a021
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-10-23 00:47:48 -0700

    return next.config.mjs file to how it was

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Enabling React Strict Mode for development (optional but recommended)
-    reactStrictMode: true,
-  
-    // Ensure Next.js uses server-based rendering (default on Vercel)
-    // This tells Next.js to use Node.js as the target, enabling SSR, ISR, and dynamic routes
-    output: 'standalone',
-  
-    // Optionally customize headers for specific features
-    async headers() {
-      return [
-        {
-          source: '/(.*)',  // Apply to all routes
-          headers: [
-            {
-              key: 'Permissions-Policy',  // Adjust or remove Permissions-Policy header
-              value: 'interest-cohort=()',  // Disable FLoC or other experimental features
-            },
-          ],
-        },
-      ];
-    },
-  };
+const nextConfig = {};
   
   export default nextConfig;
   
