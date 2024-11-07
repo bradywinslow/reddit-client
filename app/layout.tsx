ad4e46f5b9a9f8a09683d2ffea93a58b72daf7dd
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2024-11-06 22:32:53 -0800

    add title to pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import { Providers } from './providers';
 import LayoutContent from './_components/LayoutContent';
 import './globals.css';
 
+export const metadata = {
+  title: 'Reddit Client Gaming Feed'
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
