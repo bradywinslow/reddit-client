86bbcb146b6c96c3ca47e9cc647eb9ead0663f01
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-07-20 16:43:15 -0700

    make change to testing file

--- app/_reddit/IsExternalLink.test.ts
+++ app/_reddit/IsExternalLink.test.ts
@@ -19,23 +19,3 @@ describe('isExternalLink', () => {
 
     };
 });
-
-
-
-
-/*
-// Checks if url field is an external link
-export default function isExternalLink(postUrl: string) {
-    if (
-        postUrl.includes('https://') &&
-        !postUrl.endsWith('.jpg') &&
-        !postUrl.endsWith('.png') &&
-        !postUrl.includes('.reddit.com') &&
-        !postUrl.includes('.redd.it')
-    ) {
-        return true;
-    } else {
-        return false;
-    }
-}
-*/
